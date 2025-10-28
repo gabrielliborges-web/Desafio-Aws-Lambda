b55a285304e103c3ddb239e171d698aa6b481e10
fl-cci <tecnologia@ccigris.com.br>
2025-10-28 17:31:41 -0300

    chore(aws-lambda): função lambda que será usada no eventbridge aws

---
+++ projetos/cubos/aws-lambda/sendNotification/test.js
@@ -0,0 +1,7 @@
+import { handler } from "./index.js";
+
+handler({
+  to: "gabrielli.meloo16@gmail.com",
+  subject: "Teste Lambda Local",
+  message: "Se você recebeu este e-mail, está tudo certo 🚀",
+});
